c5f7af13a489f61f0efd1ca956115290be03c6eb
SASI-LAPTOP-ET9\mesas <mesasi2034@gmail.com>
2022-02-13 21:04:22 +0530

    some changes

--- ceaser_cipher.py
+++ ceaser_cipher.py
@@ -1,36 +1,12 @@
-from posixpath import split
-
-
-alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ]
+from def_functions import encrypt
+from def_functions import dycrypt
 
 directions = input("Type 'encode' to encrypt, type 'decode' to dycript: \n").lower()
 text = input("Type your messege:\n").lower()
 shift = int(input("Type the shift number:\n"))
-# TODO-1: create a function called "encrypt".
 
-def encrypt(plain_text, shift_amount):
-    cipher_text = ""
-    for letter in plain_text:
-        position = alphabets.index(letter)
-        new_position = position+shift_amount
-        if new_position >= 26:
-            new_position = 26 - new_position
-        encrypted_letter = alphabets[new_position]
-        cipher_text += encrypted_letter
-    print(f"The encoded text is {cipher_text}.")
 
-# TODO-2: create a function called "dycript".
 
-def dycrypt(cipher_text, shift_amount):
-    plain_text = ""
-    for letter in cipher_text:
-        position = alphabets.index(letter)
-        new_position = position-shift_amount
-        if new_position >= 26:
-            new_position = 26 - new_position
-        dycrypted_letter = alphabets[new_position]
-        plain_text += dycrypted_letter
-    print(f"The decoded text is {plain_text}.")
 
 if directions == "encode":
     encrypt(text, shift)
--- def_functions.py
+++ def_functions.py
@@ -1,3 +1,7 @@
+alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ]
+
+# TODO-1: create a function called "encrypt".
+
 def encrypt(plain_text, shift_amount):
     cipher_text = ""
     for letter in plain_text:
