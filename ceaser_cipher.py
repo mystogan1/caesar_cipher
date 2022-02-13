85026e6886a24204b4e85f3a9c149d10e72533ca
SASI-LAPTOP-ET9\mesas <mesasi2034@gmail.com>
2022-02-13 19:34:29 +0530

    declaring variables

---
+++ ceaser_cipher.py
@@ -0,0 +1,5 @@
+alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ]
+
+directions = input("Type 'encode' to encrypt, type 'decode' to dycript: \n").lower()
+text = input("Type your messege:\n").lower()
+shift = int(input("Type the shift number:\n"))
