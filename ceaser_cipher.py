e39418592dd5d93d353f94fed543b15bd84c069b
SASI-LAPTOP-ET9\mesas <mesasi2034@gmail.com>
2022-02-14 23:15:42 +0530

    Enhancing user experience

--- ceaser_cipher.py
+++ ceaser_cipher.py
@@ -1,5 +1,6 @@
-alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
-'m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
+from variables import *
+
+print(logo)
 
 direction = input("Type 'encode' to encrypt, type 'decode' to dycript: \n").lower()
 text = input("Type your messege:\n").lower()
