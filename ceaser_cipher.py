8f83fd5b85ac0d7f467341d1a41e8d2c0be6c03a
SASI-LAPTOP-ET9\mesas <mesasi2034@gmail.com>
2022-02-14 22:59:22 +0530

    easy sol for bug

--- ceaser_cipher.py
+++ ceaser_cipher.py
@@ -1,4 +1,6 @@
-alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
+alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 
+'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 
+'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
 
 direction = input("Type 'encode' to encrypt, type 'decode' to dycript: \n").lower()
 text = input("Type your messege:\n").lower()
@@ -7,12 +9,12 @@ shift = int(input("Type the shift number:\n"))
 
 def ceaser(start_text, shift_amount, cipher_direction):
     end_text = ""
-    shift_minus = False
+
+    if cipher_direction == "decode":
+            shift_amount *= -1
+
     for letter in start_text:
         position = alphabets.index(letter)
-        if cipher_direction == "decode" and not shift_minus:
-            shift_amount *= -1
-            shift_minus = True
         new_position = position + shift_amount
         end_text += alphabets[new_position]
     print(f"The {direction}d text is {end_text}:\n")
