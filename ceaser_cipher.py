06a35b66befd07f574f98f03164979aa730b2179
SASI-LAPTOP-ET9\mesas <mesasi2034@gmail.com>
2022-02-14 23:15:08 +0530

    enhancing

--- ceaser_cipher.py
+++ ceaser_cipher.py
@@ -1,6 +1,5 @@
-alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 
-'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 
-'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
+alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+'m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
 
 direction = input("Type 'encode' to encrypt, type 'decode' to dycript: \n").lower()
 text = input("Type your messege:\n").lower()
@@ -12,10 +11,12 @@ def ceaser(start_text, shift_amount, cipher_direction):
 
     if cipher_direction == "decode":
             shift_amount *= -1
-
+    
     for letter in start_text:
         position = alphabets.index(letter)
         new_position = position + shift_amount
+        if new_position > 26:
+            new_position = new_position%26
         end_text += alphabets[new_position]
     print(f"The {direction}d text is {end_text}:\n")
 
