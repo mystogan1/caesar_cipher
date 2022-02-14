4242aa160a7a67b5640256b802d7510119eac726
SASI-LAPTOP-ET9\mesas <mesasi2034@gmail.com>
2022-02-14 15:44:46 +0530

    Error solved

--- ceaser_cipher.py
+++ ceaser_cipher.py
@@ -7,10 +7,12 @@ shift = int(input("Type the shift number:\n"))
 
 def ceaser(start_text, shift_amount, cipher_direction):
     end_text = ""
+    shift_minus = False
     for letter in start_text:
         position = alphabets.index(letter)
-        if cipher_direction == "decode":
+        if cipher_direction == "decode" and not shift_minus:
             shift_amount *= -1
+            shift_minus = True
         new_position = position + shift_amount
         end_text += alphabets[new_position]
     print(f"The {direction}d text is {end_text}:\n")
